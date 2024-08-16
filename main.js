b200c1623a9ebd15da2595e8a593b6048bb81bad
boltrsss <163812338+boltrsss@users.noreply.github.com>
2024-08-16 23:32:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("myModal");e.style.display="block",e.classList.add("fade"),setTimeout(()=>{e.classList.add("show")},500)});
